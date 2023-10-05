642e041729cfb94aaa0bf2d92e9c65dc1ccc2df6
sbvenkatesh333@gmail.com <sbvenkatesh333@gmail.com>
2023-10-05 10:41:07 +0530

    initial commit

---
+++ msbr_llm_v1.py
@@ -0,0 +1,78 @@
+import streamlit as st
+import time
+import os
+from langchain.llms import LlamaCpp
+from langchain import PromptTemplate, LLMChain
+from langchain.callbacks.manager import CallbackManager
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from huggingface_hub import hf_hub_download
+
+
+st.title("MSBR - Threat LLM Model")
+
+model_name_or_path = "TheBloke/Llama-2-13B-chat-GGML"
+model_basename = "llama-2-13b-chat.ggmlv3.q5_1.bin"
+
+# st.write("Downloading model...")
+# model_path = hf_hub_download(repo_id=model_name_or_path, filename=model_basename)
+
+if os.path.exists(model_basename):
+    st.write("Using locally available model...")
+    model_path = model_basename
+else:
+    st.write("Downloading model...")
+    model_path = hf_hub_download(repo_id=model_name_or_path, filename=model_basename)
+
+
+# Define your Streamlit input
+question = st.text_input("Enter your Threat Component Name:")
+if st.button("Submit"):
+    st.write("Generating response...")
+    
+    response_placeholder = st.empty()
+
+    # Rest of your code
+    template = """''SYSTEM: You are an expert in cyber security.
+    USER: Give me list of 20 threat name, attack domain and threat description for {question}
+    ASSISTANT: """
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+
+    callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+
+    n_gpu_layers = 40
+    n_batch = 512
+
+    llm = LlamaCpp(
+        model_path=model_path,
+        max_tokens=1024,
+        n_gpu_layers=n_gpu_layers,
+        n_batch=n_batch,
+        callback_manager=callback_manager,
+        verbose=True,
+        n_ctx=4096,
+        stop=['USER:'],
+        temperature=0.1,
+    )
+
+    llm_chain = LLMChain(prompt=prompt, llm=llm)
+
+    response = llm_chain.run(question)
+    st.write("Response:")
+    st.write(response)
+
+
+    # Start generating the response
+    # with response_placeholder:
+    #     response_placeholder.empty()  # Clear the initial "Generating response..." message
+    #     for _ in range(5):  # You can adjust the number of iterations as needed
+    #         response = llm_chain.run(question)
+    #         st.write("Response:")
+    #         st.write(response)
+    #         time.sleep(2) 
+    
+    
+# !CMAKE_ARGS="-DLLAMA_CUBLAS=on" FORCE_CMAKE=1 pip install llama-cpp-python==0.1.78 --force-reinstall --upgrade --no-cache-dir --verbose
+# !pip install huggingface_hub streamlit langchain
+# ! pip install streamlit -q
+# !wget -q -O - ipv4.icanhazip.com
+# ! streamlit run app.py & npx localtunnel --port 8501
