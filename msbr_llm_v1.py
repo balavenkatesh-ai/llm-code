40c7195af6aa3a7ed9a162c0eb1ebb161d60ea52
sbvenkatesh333@gmail.com <sbvenkatesh333@gmail.com>
2023-12-12 10:52:25 +0530

    prompt update

--- msbr_llm_v1.py
+++ msbr_llm_v1.py
@@ -63,7 +63,7 @@ if st.button("Generate Threat"):
             5. CAPEC Reference URL: Include the URL of the CAPEC (Common Attack Pattern Enumeration and Classification) database for each threat, linking to its CAPEC page.
             6. References: Provide reference source names or URLs to verify the accuracy of the threat information provided.
                         
-            Note:- Your output should be in the format of a streamlit markdown table with the following given above columns. [/INST]
+            Note:- Your output should be in the pandas dataframe table format with the following given above columns. [/INST]
             """
             
             # Rest of your code
