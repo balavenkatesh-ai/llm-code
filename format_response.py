151c533dada07a3aaa386fe5093e0a17f795c817
sbvenkatesh333@gmail.com <sbvenkatesh333@gmail.com>
2024-02-17 15:24:40 +0530

    aded

--- format_response.py
+++ format_response.py
@@ -68,62 +68,122 @@ if st.button("Generate Threat"):
             # )
             # ]
             
-            response_schemas = [ResponseSchema(
+            response_schemas = [
+                                ResponseSchema(
                                     name="Threat Name",
-                                    description="A descriptive name for each potential threat (e.g., Data Manipulation, SQL Injection, Insecure Direct Object References, etc.)",
+                                    description="A descriptive name for each potential threat (e.g., Data Manipulation)"
                                 ),
                                 ResponseSchema(
                                     name="Threat Description",
-                                    description="Offer a concise explanation of the potential attack, detailing how attackers can exploit vulnerabilities or weaknesses to achieve their goals. For example, describe how attackers could manipulate data in the given component due to improper access controls or vulnerabilities in the application using the database. Consider referencing relevant attack examples from resources like SANS, OWASP, or MITRE ATT&CK.",
+                                    description="Offer a concise explanation of the potential attack. For example, describe how attackers can manipulate data in given component name due to improper access controls or vulnerabilities in the application using the database."
                                 ),
                                 ResponseSchema(
                                     name="Attack Domain",
-                                    description="Specify the category of attack, such as Network, Application, Data, or Physical.",
+                                    description="Specify the category of attack, such as network or application"
                                 ),
                                 ResponseSchema(
                                     name="Countermeasure",
-                                    description="Suggest recommendations to mitigate each threat and prevent successful exploitation. Be specific and actionable, referencing best practices and industry standards from organizations like CIS, NIST, or OWASP. Example: 'Implement RBAC and follow the principle of least privilege for data access control. Validate and sanitize all user input to prevent SQL injection.'",
+                                    description="Suggest recommendations to mitigate each threat"
                                 ),
-                                ResponseSchema(
-                                    name="MITRE Tactics ID",
-                                    description="Specify the corresponding MITRE Tactics ID URL from the MITRE ATT&CK® framework (e.g., https://attack.mitre.org/tactics/TA0043/), if applicable. Consider using the latest ATT&CK matrix version.",
-                                ),
-                                ResponseSchema(
-                                    name="MITRE Tactics Description",
-                                    description="Provide a brief description of the MITRE Tactics ID from the MITRE ATT&CK® framework, if applicable.",
-                                ),
-                                ResponseSchema(
-                                    name="MITRE Techniques ID",
-                                    description="Specify the relevant MITRE Techniques ID URL from the MITRE ATT&CK® framework (e.g., https://attack.mitre.org/techniques/T1548/), if applicable. Use the latest ATT&CK matrix version.",
-                                ),
-                                ResponseSchema(
-                                    name="MITRE Techniques Description",
-                                    description="Offer a concise explanation of the MITRE Techniques ID from the MITRE ATT&CK® framework, if applicable.",
-                                ),
-                                ResponseSchema(
-                                    name="CAPEC Reference URL",
-                                    description="Include the URL of the Common Attack Pattern Enumeration and Classification (CAPEC) database entry for each threat, linking to its CAPEC page (e.g., https://capec.mitre.org/data/definitions/1000.html), if applicable.",
-                                ),
-                                ResponseSchema(
+                                 ResponseSchema(
                                     name="NIST Reference",
-                                    description="Provide relevant information or recommendations from the National Institute of Standards and Technology (NIST) Special Publications (SPs) series or other sources, where applicable. For example, cite guidance from NIST SP 800-53 for security controls or SP 800-61 for incident response.",
+                                    description="Provide relevant information or recommendations from the National Institute of Standards and Technology (NIST)"
+                                ),
+                                ResponseSchema(
+                                    name="ASVS Reference",
+                                    description="Reference to the OWASP Application Security Verification Standard (ASVS)"
                                 ),
                                 ResponseSchema(
                                     name="OWASP Reference",
-                                    description="Include the URL of the relevant OWASP Top 10 entry or project page (e.g., https://owasp.org/Top10/), if applicable. Use the latest OWASP Top 10 version.",
+                                    description="Reference to the Open Web Application Security Project (OWASP)"
                                 ),
                                 ResponseSchema(
                                     name="SANS Reference",
-                                    description="Provide the URL of the corresponding SANS Institute information center page, white paper, or SANS Institute course (e.g., https://www.sans.org/reading-room/whitepapers/incident/securing-web-services-2nd-edition/), if applicable.",
+                                    description="Reference to the SANS Institute, a leading organization in cybersecurity education and training"
                                 ),
                                 ResponseSchema(
                                     name="CIS Reference",
-                                    description="Include the URL of the relevant Center for Internet Security (CIS) Controls recommendation (e.g., https://www.cisecurity.org/controls/cis-controls-v8/), if applicable.",
+                                    description="Reference to the Center for Internet Security (CIS), a nonprofit organization that provides cybersecurity resources and best practices"
                                 ),
+                                ResponseSchema(
+                                    name="MITRE Tactics ID",
+                                    description="Specify the corresponding MITRE Tactics ID URL from the MITRE ATT&CK® framework (e.g., https://attack.mitre.org/tactics/TA0043/)"
+                                ),
+                                ResponseSchema(
+                                    name="MITRE Tactics Description",
+                                    description="Provide a brief description of the MITRE Tactics ID from the MITRE ATT&CK® framework."
+                                ),
+                                ResponseSchema(
+                                    name="MITRE Techniques ID",
+                                    description="Specify the relevant MITRE Techniques ID URL from the MITRE ATT&CK® framework (e.g., https://attack.mitre.org/techniques/T1548/)"
+                                ),
+                                ResponseSchema(
+                                    name="MITRE Techniques Description",
+                                    description="Offer a concise explanation of the MITRE Techniques ID from the MITRE ATT&CK® framework"
+                                ),
+                                ResponseSchema(
+                                    name="CAPEC Reference URL",
+                                    description="Include the URL of the Common Attack Pattern Enumeration and Classification (CAPEC) database entry for each threat, linking to its CAPEC page(e.g.,https://capec.mitre.org/data/definitions/1000.html)"
+                                )
+                               
                             ]
 
             
-            
+            # response_schemas = [ResponseSchema(
+            #                         name="Threat Name",
+            #                         description="A descriptive name for each potential threat (e.g., Data Manipulation, SQL Injection, Insecure Direct Object References, etc.)",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="Threat Description",
+            #                         description="Offer a concise explanation of the potential attack, detailing how attackers can exploit vulnerabilities or weaknesses to achieve their goals. For example, describe how attackers could manipulate data in the given component due to improper access controls or vulnerabilities in the application using the database. Consider referencing relevant attack examples from resources like SANS, OWASP, or MITRE ATT&CK.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="Attack Domain",
+            #                         description="Specify the category of attack, such as Network, Application, Data, or Physical.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="Countermeasure",
+            #                         description="Suggest recommendations to mitigate each threat and prevent successful exploitation. Be specific and actionable, referencing best practices and industry standards from organizations like CIS, NIST, or OWASP. Example: 'Implement RBAC and follow the principle of least privilege for data access control. Validate and sanitize all user input to prevent SQL injection.'",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="MITRE Tactics ID",
+            #                         description="Specify the corresponding MITRE Tactics ID URL from the MITRE ATT&CK® framework (e.g., https://attack.mitre.org/tactics/TA0043/), if applicable. Consider using the latest ATT&CK matrix version.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="MITRE Tactics Description",
+            #                         description="Provide a brief description of the MITRE Tactics ID from the MITRE ATT&CK® framework, if applicable.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="MITRE Techniques ID",
+            #                         description="Specify the relevant MITRE Techniques ID URL from the MITRE ATT&CK® framework (e.g., https://attack.mitre.org/techniques/T1548/), if applicable. Use the latest ATT&CK matrix version.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="MITRE Techniques Description",
+            #                         description="Offer a concise explanation of the MITRE Techniques ID from the MITRE ATT&CK® framework, if applicable.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="CAPEC Reference URL",
+            #                         description="Include the URL of the Common Attack Pattern Enumeration and Classification (CAPEC) database entry for each threat, linking to its CAPEC page (e.g., https://capec.mitre.org/data/definitions/1000.html), if applicable.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="NIST Reference",
+            #                         description="Provide relevant information or recommendations from the National Institute of Standards and Technology (NIST) Special Publications (SPs) series or other sources, where applicable. For example, cite guidance from NIST SP 800-53 for security controls or SP 800-61 for incident response.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="OWASP Reference",
+            #                         description="Include the URL of the relevant OWASP Top 10 entry or project page (e.g., https://owasp.org/Top10/), if applicable. Use the latest OWASP Top 10 version.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="SANS Reference",
+            #                         description="Provide the URL of the corresponding SANS Institute information center page, white paper, or SANS Institute course (e.g., https://www.sans.org/reading-room/whitepapers/incident/securing-web-services-2nd-edition/), if applicable.",
+            #                     ),
+            #                     ResponseSchema(
+            #                         name="CIS Reference",
+            #                         description="Include the URL of the relevant Center for Internet Security (CIS) Controls recommendation (e.g., https://www.cisecurity.org/controls/cis-controls-v8/), if applicable.",
+            #                     ),
+            #                 ]
+
+        
             template = """ <s>[INST] <<SYS>>
             Act as a cyber security expert with more than 30 years experience of threat library development for given {component_name} {component_version}, your task is to prepare a list of {number_of_threat} threats.It is very important that your responses are tailored to reflect the details you are given.
             
