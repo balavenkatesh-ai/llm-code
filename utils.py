117ba2f81633bbe0af77c0795a7604e4ed284500
balavenkatesh-ai <86469976+balavenkatesh-ai@users.noreply.github.com>
2024-07-15 13:44:43 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,129 +1,18 @@
-# Ignore data folder
-data/
-
-# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-*$py.class
-
-# C extensions
-*.so
-
-# Distribution / packaging
-.Python
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-wheels/
-share/python-wheels/
-*.egg-info/
-.installed.cfg
-*.egg
-
-# PyInstaller
-#  Usually these files are written by a python script from a template
-#  before PyInstaller builds the exe, so as to inject date/other infos into it.
-*.manifest
-*.spec
-
-# Installer logs
-pip-log.txt
-pip-delete-this-directory.txt
-
-# Unit test / coverage reports
-htmlcov/
-.tox/
-.nox/
-.coverage
-.coverage.*
-.cache
-nosetests.xml
-coverage.xml
-*.cover
-.hypothesis/
-.pytest_cache/
-cover/
-
-# Translations
-*.mo
-*.pot
-
-# Django stuff:
-*.log
-local_settings.py
-db.sqlite3
-db.sqlite3-journal
-
-# Flask stuff:
-instance/
-.webassets-cache
-
-# Scrapy stuff:
-.scrapy
-
-# Sphinx documentation
-docs/_build/
-
-# PyBuilder
-target/
-
-# Jupyter Notebook
-.ipynb_checkpoints
-
-# IPython
-profile_default/
-ipython_config.py
-
-# pyenv
-.python-version
-
-# celery beat schedule file
-celerybeat-schedule
-celerybeat.pid
-
-# SageMath parsed files
-*.sage.py
-
-# Environments
-.env
-.venv
-env/
-venv/
-ENV/
-env.bak/
-venv.bak/
-
-# Spyder project settings
-.spyderproject
-.spyproject
-
-# Rope project settings
-.ropeproject
-
-# mkdocs documentation
-/site
-
-# mypy
-.mypy_cache/
-.dmypy.json
-dmypy.json
-
-# Pyre type checker
-.pyre/
-
-# pytype static type analyzer
-.pytype/
-
-# Cython debug symbols
-cython_debug/
-
-# VS Code
-.vscode/
+def get_db_url() -> str:
+    """
+    Get the database URL from the system environment variable.
+    
+    :return: Database URL string.
+    """
+    db_url = os.getenv('TIP_ICS_POSTGRES_DB_URL')
+    if db_url is None:
+        raise ValueError("TIP_ICS_POSTGRES_DB_URL environment variable is not set.")
+    
+    # URL encode username and password
+    db_url_parts = db_url.split('@')
+    if len(db_url_parts) != 2:
+        raise ValueError("Invalid DATABASE_URL format.")
+    
+    user_info, rest = db_url_parts
+    user_info_encoded = quote(user_info, safe=':')
+    return f"{user_info_encoded}@{rest}"
