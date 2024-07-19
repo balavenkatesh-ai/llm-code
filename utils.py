efae723e8ab22185f71edfc7504f5ee49dfed2ce
balavenkatesh-ai <86469976+balavenkatesh-ai@users.noreply.github.com>
2024-07-19 16:24:29 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,3 +1,18 @@
+from sqlalchemy import Column, Integer, DateTime, String
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from datetime import datetime
+
+@as_declarative()
+class Base:
+    id: int = Column(Integer, primary_key=True, index=True)
+    created_by: str = Column(String, nullable=True)
+    created_at: datetime = Column(DateTime, default=datetime.utcnow)
+    updated_by: str = Column(String, nullable=True)
+    updated_at: datetime = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+
+
+
 from sqlalchemy import Column, String, Integer, JSON, create_engine
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 from sqlalchemy.ext.declarative import declarative_base
